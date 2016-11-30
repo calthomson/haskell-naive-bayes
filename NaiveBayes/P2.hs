a80736bd5e8d480b0f3d18b099466ff2e15002ab
calthomson <caledoniathomson@gmail.com>
2016-11-29 18:13:15 -0800

    Load in CSV file and parse into a vector of 10 floats and ints

--- NaiveBayes/P2.hs
+++ NaiveBayes/P2.hs
@@ -4,6 +4,15 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
 
+-- text
+import Data.Text (Text)
+import qualified Data.Text.Encoding as Text
+
+-- vector
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+
+-- "Classes" of our data
 --   1. Number of times pregnant
 --   2. Plasma glucose concentration a 2 hours in an oral glucose tolerance test
 --   3. Diastolic blood pressure (mm Hg)
@@ -14,10 +23,39 @@ import qualified Data.Vector as V
 --   8. Age (years)
 --   9. Class variable (0 or 1)
 
+-- Original attempt at reading in file
+-- loadCsv :: IO ()
+-- loadCsv = do
+--   csvData <- BL.readFile "data.csv"
+--   case decode NoHeader csvData of
+--       Left err -> putStrLn err
+--       Right V.forM_ v $ \ (pregnancies, plasmaGlucose, bloodPressure, tricepFolds, serumInsulin, bmi, diabetesPedigree, age, diabetic) ->
+ --           putStrLn $ pregnancies ++ ", " ++ plasmaGlucose ++ ", " ++ bloodPressure ++ ", " ++ tricepFolds ++ ", " ++ serumInsulin ++ ", " ++ bmi ++ ", " ++ diabetesPedigree ++ ", " ++ age ++ ", " ++ diabetic
+
+-- THE BELOW CODE IS FROM HERE: https://howistart.org/posts/haskell/1
+-- a simple type alias for data
+type Patient = (Int, Int, Int, Int, Int, Float, Float, Int, Int)
+
 main :: IO ()
 main = do
-    csvData <- BL.readFile "data.csv"
-    case decode NoHeader csvData of
-        Left err -> putStrLn err
-        Right v -> V.forM_ v $ \ (pregnancies, plasmaGlucose, bloodPressure, tricepFolds, serumInsulin, bmi, diabetesPedigree, age, diabetic) ->
-            putStrLn $ pregnancies ++ ", " ++ plasmaGlucose ++ ", " ++ bloodPressure ++ ", " ++ tricepFolds ++ ", " ++ serumInsulin ++ ", " ++ bmi ++ ", " ++ diabetesPedigree ++ ", " ++ age ++ ", " ++ diabetic
+  csvData <- BL.readFile "data.csv" 
+  let v = decode NoHeader csvData :: Either String (V.Vector Patient)
+  -- let summed = fmap (V.foldr summer 0) v
+  -- putStrLn $ "Total atBats was: " ++ (show summed)
+  print v
+  -- where summer (name, year, team, atBats) n = n + atBats
+
+-- Attempt at creating patient item (Maybe we should change the above 'type' to look like this?)
+--data Item =
+--  Item
+--    { pregnancies :: Text
+--    , plasmaGlucose :: Text
+--    , bloodPressure :: Text
+--    , tricepFolds :: Text
+--    , serumInsulin :: Text
+--    , bmi :: Text
+--    , diabetesPedigree :: Text
+--    , age :: Text
+--    , diabetic :: Bool
+--    }
+--  deriving (Eq, Show)
