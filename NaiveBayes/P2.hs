2656fb39b870c283d552f5fc98de168b5abfb8d2
CallumCampbell <caljcamp@gmail.com>
2016-12-01 20:23:39 -0800

    Data parsing and summary

--- NaiveBayes/P2.hs
+++ NaiveBayes/P2.hs
@@ -30,7 +30,8 @@ import qualified Data.Text.Encoding as Text
 
 -- THE BELOW CODE IS FROM HERE: https://howistart.org/posts/haskell/1
 -- a simple type alias for data
-type Patient = (Int, Int, Int, Int, Int, Float, Float, Int, Int)
+--type Patient = (Int, Int, Int, Int, Int, Float, Float, Int, Int)
+type Patient = (Float, Float, Float, Float, Float, Float, Float, Float, Float)
 
 --type mytype =  Either String (V.Vector Patient)
 main :: IO ()
@@ -38,15 +39,40 @@ main = do
   csvData <- BL.readFile "data.csv" 
   case decode NoHeader csvData of
     Left err -> putStrLn err
-    Right v -> print (separateByClass v)
+    Right v -> do
+        let sep = (separateByClass v)
+        let summaries = summarizeTuple sep 
+        print summaries
     
-  --let v = decode NoHeader csvData :: Either String (V.Vector Patient)
-  -- let summed = fmap (V.foldr summer 0) v
-  -- putStrLn $ "Total atBats was: " ++ (show summed)
-  --print (separateByClass v)
-  -- where summer (name, year, team, atBats) n = n + atBats
+  
 
-
+-- Partitions vector a tuple of two vectors, one containing all diabetes positive cases, the other with the negatives
 separateByClass :: (V.Vector Patient) -> ((V.Vector Patient), (V.Vector Patient))
-separateByClass vec = ((V.filter (\(_,_,_,_,_,_,_,_,x) -> x==1) vec) , (V.filter (\(_,_,_,_,_,_,_,_,x) -> x==0) vec))
+separateByClass vec = (V.partition (\(_,_,_,_,_,_,_,_,x) -> x==1) vec) 
 
+-- Summarizes the diabetes positive and negative vectors
+summarizeTuple (v1, v2) = (summarize v1, summarize v2) 
+
+-- returns the means and standard devations for each 'column' of our vectors
+summarize :: (V.Vector Patient) -> [(Float,Float)]
+summarize v =
+    let 
+        (v1, v2, v3, v4 ,v5 ,v6, v7, v8) = (splitVars v)
+        list = v1:v2:v3:v4:v5:v6:v7:v8:[]
+        means = map mean list
+        stdevs = map stdev list
+    in (zip means stdevs)
+    
+
+-- Splits a vector of tuples into a tuple of list [(v1..v9)..] -> (l1, .., l8) where li is the list of vi's and we ignore the final variable because we already split based on that being 1 or 0 
+splitVars v = V.foldr myFun ([],[],[],[],[],[],[],[]) v
+myFun (pregnancies, plasmaGlucose, bloodPressure, tricepFolds, serumInsulin, bmi, diabetesPedigree, age, diabetes) (lst1, lst2, lst3, lst4, lst5, lst6, lst7, lst8) = ((pregnancies:lst1), (plasmaGlucose:lst2), (bloodPressure:lst3), (tricepFolds:lst4), (serumInsulin:lst5), (bmi:lst6), (diabetesPedigree:lst7), (age:lst8))      
+
+-- Calculates  mean and stardard devation of a list of floats
+mean numbers = (sum numbers)  / fromIntegral(length numbers)
+stdev :: [Float] -> Float
+stdev numbers = 
+    let 
+        avg = mean numbers
+        varience = (sum [(x-avg)^(fromIntegral(2)) | x <- numbers])/ fromIntegral(length(numbers)-1)
+    in (sqrt varience)
